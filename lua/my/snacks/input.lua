319bba46cdce3429b9cf1c423e465ac8b92c4ed4
Brian Rodriguez <brian@brianrodri.com>
2025-06-02 15:10:19 -0400

    chore: use default input window positioning

--- lua/my/snacks/input.lua
+++ lua/my/snacks/input.lua
@@ -3,9 +3,6 @@ return {
   ---@type snacks.input.Config|{}
   config = {
     win = {
-      relative = "cursor",
-      row = -3,
-      col = 0,
       keys = {
         i_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "i", expr = true },
         ctrl_f = { "<c-f>", { "cmp_close", "<esc>" }, mode = "i", expr = true },
