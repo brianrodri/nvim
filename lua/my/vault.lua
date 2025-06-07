dc5d11572807d8b07b7ea4631f38443760fce259
Brian Rodriguez <brian@brianrodri.com>
2025-06-06 20:49:54 -0400

    fix(my.vault): only pass vault-relative paths to obsidian.nvim

--- lua/my/vault.lua
+++ lua/my/vault.lua
@@ -1,8 +1,10 @@
-local ROOT_DIR = vim.fs.normalize(vim.fs.joinpath("~", "Documents", "Vault"))
-local INBOX_NOTE = vim.fs.joinpath(ROOT_DIR, "0 - Index", "Inbox.md")
-local DAILY_NOTES_DIR = vim.fs.joinpath(ROOT_DIR, "1 - Journal", "Daily")
-local FLEETING_NOTES_DIR = vim.fs.joinpath(ROOT_DIR, "2 - Fleeting Notes")
-local ATTACHMENTS_DIR = vim.fs.joinpath(ROOT_DIR, "8 - Meta", "Attachments")
+local my_env = require("my.env")
+
+local ROOT_DIR = vim.fs.normalize(vim.fs.joinpath("~", "Documents", "Vault"), { win = my_env.wsl_mode })
+local INBOX_NOTE = vim.fs.joinpath("0 - Index", "Inbox.md")
+local DAILY_NOTES_DIR = vim.fs.joinpath("1 - Journal", "Daily")
+local FLEETING_NOTES_DIR = vim.fs.joinpath("2 - Fleeting Notes")
+local ATTACHMENTS_DIR = vim.fs.joinpath("8 - Meta", "Attachments")
 
 return {
   root_dir = ROOT_DIR,
@@ -10,15 +12,21 @@ return {
 
   actions = {
     ---@param note_path string
-    open_note = function(note_path) require("obsidian").get_client():open_note(note_path) end,
+    open_note = function(note_path)
+      local client = require("obsidian").get_client()
+      local dest_note = assert(client:resolve_note(note_path), "unable to resolve note: " .. note_path)
+      client:open_note(dest_note, { sync = true })
+    end,
 
     ---@param note_path string
     ---@param default? string
     open_note_in_append_mode = function(note_path, default)
-      default = default or ""
-      require("obsidian").get_client():open_note(note_path, {
+      local client = require("obsidian").get_client()
+      local dest_note = assert(client:resolve_note(note_path), "unable to resolve note: " .. note_path)
+      client:open_note(dest_note, {
+        sync = true,
         callback = function()
-          vim.cmd("normal! Go" .. default)
+          vim.cmd("normal! Go" .. (default or ""))
           vim.cmd("startinsert!")
         end,
       })
@@ -33,7 +41,10 @@ return {
         { prompt = string.format("Append To %s", dest_note:display_name()), default = default },
         function(text)
           if vim.fn.empty(text) == 1 then return end
-          client:write_note(dest_note, { update_content = function(lines) return vim.list_extend(lines, { text }) end })
+          client:write_note(dest_note, {
+            sync = true,
+            update_content = function(lines) return vim.list_extend(lines, { text }) end,
+          })
           local curr_note = client:current_note()
           if curr_note and curr_note.path == dest_note.path then vim.schedule(function() vim.cmd("bufdo e") end) end
         end
