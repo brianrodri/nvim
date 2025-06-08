0025f15f8c81141a60ed1c7a0a82efcbb1d36d2c
Brian Rodriguez <brian@brianrodri.com>
2025-06-08 09:40:59 -0400

    style: align which-key groups

--- lua/plugins/qol/which-key.lua
+++ lua/plugins/qol/which-key.lua
@@ -7,23 +7,24 @@ return {
   ---@module "which-key"
   ---@class wk.Opts
   opts = {
+    -- stylua: ignore
     spec = {
       -- Groups
-      { "<leader>a", group = "ai", icon = { icon = "󰚩 ", hl = "WhichKeyIconBlue" } },
-      { "<leader>b", group = "buffer", icon = { icon = "󰈔 ", hl = "WhichKeyIconCyan" } },
-      { "<leader>c", group = "code", icon = { icon = " ", hl = "WhichKeyIconOrange" } },
-      { "<leader>d", group = "debug", icon = { icon = "󰃤 ", hl = "WhichKeyIconRed" } },
-      { "<leader>f", group = "find", icon = { icon = "󰈞", hl = "WhichKeyIconCyan" } },
-      { "<leader>g", group = "git", icon = { icon = " ", hl = "WhichKeyIconGrey" } },
-      { "<leader>l", group = "lazy", icon = { icon = "󰒲 ", hl = "WhichKeyIconCyan" } },
-      { "<leader>n", group = "notes", icon = { icon = "󰠮 ", hl = "WhichKeyIconPurple" } },
-      { "<leader>o", group = "toggle", icon = { icon = " ", hl = "WhichKeyIconYellow" } },
-      { "<leader>p", group = "project", icon = { icon = " ", hl = "WhichKeyIconYellow" } },
-      { "<leader>q", group = "quit", icon = { icon = "󰈆 ", hl = "WhichKeyIconRed" } },
-      { "<leader>r", group = "refactor" },
-      { "<leader>s", group = "search", icon = { icon = "󱎸 ", hl = "WhichKeyIconGreen" } },
-      { "<leader>t", group = "test", icon = { icon = "󰙨 ", hl = "WhichKeyIconGreen" } },
-      { "<leader>x", group = "trouble", icon = { icon = "󱍼 ", hl = "WhichKeyIconRed" } },
+      { "<leader>a", group = "ai",       icon = { icon = "󰚩 ", hl = "WhichKeyIconBlue" } },
+      { "<leader>b", group = "buffer",   icon = { icon = "󰈔 ", hl = "WhichKeyIconCyan" } },
+      { "<leader>c", group = "code",     icon = { icon = " ", hl = "WhichKeyIconOrange" } },
+      { "<leader>d", group = "debug",    icon = { icon = "󰃤 ", hl = "WhichKeyIconRed" } },
+      { "<leader>f", group = "find",     icon = { icon = "󰥨 ", hl = "WhichKeyIconCyan" } },
+      { "<leader>g", group = "git",      icon = { icon = " ", hl = "WhichKeyIconGrey" } },
+      { "<leader>l", group = "lazy",     icon = { icon = "󰒲 ", hl = "WhichKeyIconCyan" } },
+      { "<leader>n", group = "notes",    icon = { icon = "󰠮 ", hl = "WhichKeyIconPurple" } },
+      { "<leader>o", group = "toggle",   icon = { icon = " ", hl = "WhichKeyIconYellow" } },
+      { "<leader>p", group = "project",  icon = { icon = " ", hl = "WhichKeyIconYellow" } },
+      { "<leader>q", group = "quit",     icon = { icon = "󰈆 ", hl = "WhichKeyIconRed" } },
+      { "<leader>r", group = "refactor", icon = { icon = "󰛔 ", hl = "WhichKeyIconGreen󰛔" } },
+      { "<leader>s", group = "search",   icon = { icon = "󱎸 ", hl = "WhichKeyIconGreen" } },
+      { "<leader>t", group = "test",     icon = { icon = "󰙨 ", hl = "WhichKeyIconGreen" } },
+      { "<leader>x", group = "trouble",  icon = { icon = "󱍼 ", hl = "WhichKeyIconRed" } },
 
       -- Hidden keymaps
       { "<leader><C-h>", ":leftabove vsplit<CR>", hidden = true },
