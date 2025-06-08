2d1c343a82277638db861a47ce402688cc93ff17
Brian Rodriguez <brian@brianrodri.com>
2025-06-08 08:19:03 -0400

    chore: uninstall icon-picker and use snacks.picker

--- lua/plugins/qol/icon-picker.lua
+++
@@ -1,9 +0,0 @@
----@module "lazy"
----@type LazySpec
-return {
-  "ziontee113/icon-picker.nvim",
-  opts = { disable_legacy_commands = true },
-  keys = {
-    { "<leader>si", "<cmd>IconPickerYank<cr>", desc = "Search Icons" },
-  },
-}
