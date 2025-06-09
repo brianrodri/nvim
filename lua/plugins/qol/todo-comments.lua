91e86dba5c0a9e53820c30eb8bd92f6f77f84d70
Brian Rodriguez <brian@brianrodri.com>
2025-06-09 19:42:03 -0400

    refactor: inline todo-comments config

--- lua/plugins/qol/todo-comments.lua
+++ lua/plugins/qol/todo-comments.lua
@@ -1,5 +1,3 @@
-local my_todo_comments = require("my.todo_comments")
-
 ---@module "lazy"
 ---@type LazySpec
 return {
@@ -10,8 +8,43 @@ return {
   ---@type TodoOptions|{}
   opts = {
     signs = false,
-    colors = my_todo_comments.colors,
-    keywords = my_todo_comments.keywords,
+    -- stylua: ignore
+    colors = {
+      NOTE     = "RenderMarkdownInfo",
+      ABSTRACT = "RenderMarkdownSuccess",
+      INFO     = "RenderMarkdownInfo",
+      TODO     = "RenderMarkdownInfo",
+      TIP      = "RenderMarkdownSuccess",
+      SUCCESS  = "RenderMarkdownHint",
+      QUESTION = "RenderMarkdownWarn",
+      WARNING  = "RenderMarkdownWarn",
+      FAILURE  = "RenderMarkdownError",
+      DANGER   = "RenderMarkdownError",
+      BUG      = "RenderMarkdownError",
+      EXAMPLE  = "Purple",
+      QUOTE    = "RenderMarkdownQuote",
+      TEST     = "RenderMarkdownSuccess",
+      PERF     = "RenderMarkdownSuccess",
+    },
+    --- Inspired by <https://help.obsidian.md/callouts#Supported+types>
+    -- stylua: ignore
+    keywords = {
+      NOTE     = { icon = "󰲶", color = "NOTE"                                                   },
+      ABSTRACT = { icon = "󰨸", color = "ABSTRACT", alt = { "SUMMARY", "TLDR" }                  },
+      INFO     = { icon = "󰋽", color = "INFO"                                                   },
+      TODO     = { icon = "󰗡", color = "TODO"                                                   },
+      TIP      = { icon = "󰈸", color = "TIP",      alt = { "HINT", "IMPORTANT" }                },
+      SUCCESS  = { icon = "󰄬", color = "SUCCESS",  alt = { "CHECK", "DONE" }                    },
+      QUESTION = { icon = "󰘥", color = "QUESTION", alt = { "HELP", "FAQ" }                      },
+      WARNING  = { icon = "󰀪", color = "WARNING",  alt = { "CAUTION", "ATTENTION", "WARN" }     },
+      FAILURE  = { icon = "󰅖", color = "FAILURE",  alt = { "FAIL", "MISSING" }                  },
+      DANGER   = { icon = "󱐌", color = "DANGER",   alt = { "ERROR", "HACK", "XXX" }             },
+      BUG      = { icon = "󰨰", color = "BUG",      alt = { "FIX", "FIXIT", "FIXME", "ISSUE" }   },
+      EXAMPLE  = { icon = "󰉹", color = "EXAMPLE"                                                },
+      QUOTE    = { icon = "󱆨", color = "QUOTE",    alt = { "CITE" }                             },
+      TEST     = { icon = "󰂖", color = "TEST",     alt = { "FAILED", "PASSED", "TESTING" }      },
+      PERF     = { icon = "󰅒", color = "PERF",     alt = { "OPTIM", "OPTIMIZE", "PERFORMANCE" } },
+    },
     merge_keywords = false,
     highlight = { pattern = { [[.*<(KEYWORDS):]], [[.*<(KEYWORDS)\(.*\):]] } },
     search = { pattern = "\\s(KEYWORDS)[:\\(]" },
@@ -21,6 +54,5 @@ return {
   keys = {
     { "<leader>xt", "<cmd>Trouble todo toggle filter = { tag = { DANGER, BUG, PERF, TODO } }<cr>", desc = "Todo (danger/bug/perf/todo)" },
     { "<leader>xT", "<cmd>Trouble todo toggle<cr>", desc = "Todo (all)" },
-  }
-,
+  },
 }
