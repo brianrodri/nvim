629d7b592632d83967ae496882c2670afc17c001
Brian Rodriguez <brian@brianrodri.com>
2025-06-09 19:15:33 -0400

    refactor: inline vault actions and avoid capitalized variables

--- lua/my/vault.lua
+++ lua/my/vault.lua
@@ -1,73 +1,9 @@
 local my_env = require("my.env")
 
-local ROOT_DIR = vim.fs.normalize(vim.fs.joinpath("~", "Documents", "Vault"), { win = my_env.wsl_mode })
-local INBOX_NOTE = vim.fs.joinpath("0 - Index", "Inbox.md")
-local DAILY_NOTES_DIR = vim.fs.joinpath("1 - Journal", "Daily")
-local FLEETING_NOTES_DIR = vim.fs.joinpath("2 - Fleeting Notes")
-local ATTACHMENTS_DIR = vim.fs.joinpath("8 - Meta", "Attachments")
-
 return {
-  root_dir = ROOT_DIR,
-
-  actions = {
-    ---@param note_path string
-    open_note = function(note_path)
-      local client = require("obsidian").get_client()
-      local dest_note = assert(client:resolve_note(note_path), "unable to resolve note: " .. note_path)
-      client:open_note(dest_note, { sync = true })
-    end,
-
-    ---@param note_path string
-    ---@param default? string
-    open_note_in_append_mode = function(note_path, default)
-      local client = require("obsidian").get_client()
-      local dest_note = assert(client:resolve_note(note_path), "unable to resolve note: " .. note_path)
-      client:open_note(dest_note, {
-        sync = true,
-        callback = function()
-          vim.cmd("normal! Go" .. (default or ""))
-          vim.cmd("startinsert!")
-        end,
-      })
-    end,
-
-    ---@param note_path string
-    ---@param default? string
-    append_prompt_to_note = function(note_path, default)
-      local client = require("obsidian").get_client()
-      local dest_note = assert(client:resolve_note(note_path), "unable to resolve note: " .. note_path)
-      require("snacks.input").input(
-        { prompt = string.format("Append To %s", dest_note:display_name()), default = default },
-        function(text)
-          if vim.fn.empty(text) == 1 then return end
-          client:write_note(dest_note, {
-            sync = true,
-            update_content = function(lines) return vim.list_extend(lines, { text }) end,
-          })
-          local curr_note = client:current_note()
-          if curr_note and curr_note.path == dest_note.path then vim.schedule(function() vim.cmd("bufdo e") end) end
-        end
-      )
-    end,
-  },
-
-  ---@module "obsidian"
-  ---@type obsidian.workspace.WorkspaceSpec|{}
-  personal = {
-    name = "Vault",
-    path = ROOT_DIR,
-    strict = true,
-    ---@type obsidian.config.ClientOpts|{}
-    overrides = {
-      notes_subdir = FLEETING_NOTES_DIR,
-      new_notes_location = "notes_subdir",
-      note_id_func = function(title) return title end,
-      disable_frontmatter = true,
-      ---@type obsidian.config.AttachmentsOpts|{}
-      attachments = { img_folder = ATTACHMENTS_DIR },
-      ---@type obsidian.config.DailyNotesOpts|{}
-      daily_notes = { folder = DAILY_NOTES_DIR, template = "Daily Template.md", workdays_only = false },
-    },
-  },
   inbox_note_path = vim.fs.joinpath("0 - Index", "Inbox.md"),
+  root_dir = vim.fs.normalize(vim.fs.joinpath("~", "Documents", "Vault"), { win = my_env.wsl_mode }),
+  daily_notes_dir = vim.fs.joinpath("1 - Journal", "Daily"),
+  fleeting_notes_dir = vim.fs.joinpath("2 - Fleeting Notes"),
+  attachments_dir = vim.fs.joinpath("8 - Meta", "Attachments"),
 }
--- lua/plugins/integ/obsidian/obsidian.lua
+++ lua/plugins/integ/obsidian/obsidian.lua
@@ -1,5 +1,21 @@
 local my_vault = require("my.vault")
 
+local function append_prompt_to_inbox()
+  vim.ui.input({ prompt = "Append To Inbox", default = "- " }, function(text)
+    if vim.fn.empty(text) == 1 then return end
+    local client = require("obsidian").get_client()
+    local inbox_note = client:resolve_note(my_vault.inbox_note_path)
+    client:write_note(inbox_note, { update_content = function(lst) return vim.list_extend(lst, { text }) end })
+    local curr_note = client:current_note()
+    if curr_note and curr_note.path == inbox_note.path then vim.schedule(function() vim.cmd("bufdo e") end) end
+  end)
+end
+
+local function open_inbox()
+  local client = require("obsidian").get_client()
+  client:open_note(client:resolve_note(my_vault.inbox_note_path), { sync = true })
+end
+
 ---@module "lazy"
 ---@type LazySpec
 return {
@@ -10,7 +26,24 @@ return {
     ---@type obsidian.config.ClientOpts|{}
     opts = {
       ui = { enable = false },
-      workspaces = { my_vault.personal },
+      workspaces = {
+        {
+          name = "My Vault",
+          path = my_vault.root_dir,
+          strict = true,
+          ---@type obsidian.config.ClientOpts|{}
+          overrides = {
+            notes_subdir = my_vault.fleeting_notes_dir,
+            new_notes_location = "notes_subdir",
+            note_id_func = function(title) return title end,
+            disable_frontmatter = true,
+            ---@type obsidian.config.AttachmentsOpts|{}
+            attachments = { img_folder = my_vault.attachments_dir },
+            ---@type obsidian.config.DailyNotesOpts|{}
+            daily_notes = { folder = my_vault.daily_notes_dir, template = "Daily Template.md", workdays_only = false },
+          },
+        },
+      },
       ---@type obsidian.config.OpenOpts|{}
       open = { use_advanced_uri = true },
       ---@type obsidian.config.CompletionOpts|{}
@@ -23,9 +56,8 @@ return {
       { "<leader>nt", function() require("obsidian").get_client():command("today", { args = "" }) end, desc = "Open Today's Note" },
       { "<leader>n/", function() require("snacks.picker").grep({ cwd = my_vault.root_dir }) end, desc = "Grep Notes" },
       { "<leader>ns", function() require("snacks.picker").files({ cwd = my_vault.root_dir }) end, desc = "Find Notes" },
-      { "<leader>na", function() my_vault.actions.append_prompt_to_note(my_vault.inbox_note) end, desc = "Append To Inbox" },
-      { "<leader>no", function() my_vault.actions.open_note(my_vault.inbox_note) end, desc = "Open Inbox" },
-      { "<leader>nO", function() my_vault.actions.open_note_in_append_mode(my_vault.inbox_note, "- ") end, desc = "Append To Inbox" },
+      { "<leader>na", append_prompt_to_inbox, desc = "Append To Inbox" },
+      { "<leader>no", open_inbox, desc = "Open Inbox" },
     },
   },
 }
