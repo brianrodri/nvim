fa3439a7621f4efb2767a0ad201ebe14fe374b20
Brian Rodriguez <brian@brianrodri.com>
2025-06-11 01:06:34 -0400

    refactor(plugins): move nvim-specific configs into integ folder

--- lua/plugins/integ/nvim/plenary.lua
+++ lua/plugins/integ/nvim/plenary.lua
@@ -2,6 +2,11 @@
 ---@type LazySpec
 return {
   {
+    "nvim-lua/plenary.nvim",
+    lazy = false,
+  },
+
+  {
     "nvim-neotest/neotest",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-neotest/neotest-plenary" },
     ---@module "neotest"
