e8ad93d51882b8fffe81b313b7aa04f9a7712529
Brian Rodriguez <brian@brianrodri.com>
2025-06-12 16:16:46 -0400

    feat: add keymap for toggling git signs

--- lua/plugins/integ/git/gitsigns.lua
+++ lua/plugins/integ/git/gitsigns.lua
@@ -6,6 +6,19 @@ local gitsigns_diff = {
   end,
 }
 
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyDone",
+  once = true,
+  callback = function()
+    local enabled = require("gitsigns.config").config.signcolumn
+    require("snacks.toggle")({
+      name = "GitSigns",
+      get = function() return enabled end,
+      set = function(value) enabled = require("gitsigns").toggle_signs(value) end,
+    }):map("<leader>og")
+  end,
+})
+
 ---@module "lazy"
 ---@type LazySpec
 return {
@@ -17,22 +30,32 @@ return {
       attach_to_untracked = true,
       watch_gitdir = { enable = true },
     },
-    -- stylua: ignore
-    -- luacheck: no max line length
-    keys = {
-      { "]g", function() require("gitsigns").nav_hunk("next") end, desc = "Jump To Next Hunk" },
-      { "[g", function() require("gitsigns").nav_hunk("prev") end, desc = "Jump To Previous Hunk" },
-      { "<leader>ga", function() require("gitsigns").stage_hunk() end, desc = "Stage Hunk" },
-      { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk" },
-      { "<leader>gA", function() require("gitsigns").stage_buffer() end, desc = "Stage Buffer" },
-      { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Reset Buffer" },
-      { "<leader>gK", function() require("gitsigns").preview_hunk() end, desc = "Preview Hunk" },
-      { "<leader>gd", function() require("gitsigns").diffthis() end, desc = "Diff Against Index" },
-      { "<leader>gD", function() require("gitsigns").diffthis("main") end, desc = "Diff Against Main" },
-      { "<leader>gb", function() require("gitsigns").toggle_current_line_blame() end, desc = "Toggle Blame" },
-      { "<leader>ga", function() require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, desc = "Stage Hunk", mode = "v" },
-      { "<leader>gr", function() require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, desc = "Reset Hunk", mode = "v" },
-    },
+    keys = function(_, keys)
+      return vim.list_extend(keys or {}, {
+        { "]g", function() require("gitsigns").nav_hunk("next") end, desc = "Jump To Next Hunk" },
+        { "[g", function() require("gitsigns").nav_hunk("prev") end, desc = "Jump To Previous Hunk" },
+        { "<leader>ga", function() require("gitsigns").stage_hunk() end, desc = "Stage Hunk" },
+        { "<leader>gr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk" },
+        { "<leader>gA", function() require("gitsigns").stage_buffer() end, desc = "Stage Buffer" },
+        { "<leader>gR", function() require("gitsigns").reset_buffer() end, desc = "Reset Buffer" },
+        { "<leader>gK", function() require("gitsigns").preview_hunk() end, desc = "Preview Hunk" },
+        { "<leader>gd", function() require("gitsigns").diffthis() end, desc = "Diff Against Index" },
+        { "<leader>gD", function() require("gitsigns").diffthis("main") end, desc = "Diff Against Main" },
+        { "<leader>gb", function() require("gitsigns").toggle_current_line_blame() end, desc = "Toggle Blame" },
+        {
+          "<leader>ga",
+          function() require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+          desc = "Stage Hunk",
+          mode = "v",
+        },
+        {
+          "<leader>gr",
+          function() require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+          desc = "Reset Hunk",
+          mode = "v",
+        },
+      })
+    end,
   },
 
   {
