4164fad7eb805133be9a5eee953267f775e7cb4a
Brian Rodriguez <brian@brianrodri.com>
2025-06-11 23:52:11 -0400

    refactor: use common module definition syntax

--- lua/my/paths.lua
+++ lua/my/paths.lua
@@ -1,18 +1,25 @@
-local has_wsl = vim.fn.has("wsl") == 1
+local M = {}
 
-local function resolve_path(path) return assert(vim.uv.fs_realpath(tostring(path))) end
+---@param base_path string  Resolved by composing |vim.fs.normalize()| with |uv.fs_realpath()|.
+---@param ... string        Concatenated to the **resolved path** with |vim.fs.joinpath|.
+---@return string resolved_path
+function M.resolve(base_path, ...)
+  local has_wsl = vim.fn.has("wsl") == 1
+  local resolved_root = assert(vim.uv.fs_realpath(vim.fs.normalize(tostring(base_path), { win = has_wsl })))
+  return vim.fs.joinpath(resolved_root, ...)
+end
 
-return {
-  nvim_config_dir = resolve_path(vim.fn.stdpath("config")),
-  nvim_config_plugins_dir = resolve_path(vim.fs.joinpath(vim.fn.stdpath("config"), "lua", "plugins")),
-  dot_config_dir = resolve_path(vim.fs.normalize("~/Repositories/config", { win = has_wsl })),
-  lazy_plugins_dir = resolve_path(vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")),
+M.nvim_config_dir = M.resolve(vim.fn.stdpath("config"))
+M.nvim_config_plugins_dir = M.resolve(M.nvim_config_dir, "lua", "plugins")
+M.dot_config_dir = M.resolve("~/Repositories/config")
+M.lazy_plugins_dir = M.resolve(vim.fn.stdpath("data"), "lazy")
 
-  personal_vault = {
-    root_dir = resolve_path(vim.fs.normalize("~/Documents/Vault", { win = has_wsl })),
-    inbox_note_path = "0 - Index/Inbox.md",
-    daily_notes_dir = "1 - Journal/Daily",
-    fleeting_notes_dir = "2 - Fleeting Notes",
-    attachments_dir = "8 - Meta/Attachments",
-  },
+M.personal_vault = {
+  root_dir = M.resolve("~/Documents/Vault"),
+  inbox_note_path = "0 - Index/Inbox.md",
+  daily_notes_dir = "1 - Journal/Daily",
+  fleeting_notes_dir = "2 - Fleeting Notes",
+  attachments_dir = "8 - Meta/Attachments",
 }
+
+return M
