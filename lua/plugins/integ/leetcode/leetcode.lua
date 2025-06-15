121936c38a2b91df0b9cc9e1988ab38e67b5e920
Brian Rodriguez <brian@brianrodri.com>
2025-06-15 01:25:25 -0400

    style(leetcode): personalize theme

--- lua/plugins/integ/leetcode/leetcode.lua
+++ lua/plugins/integ/leetcode/leetcode.lua
@@ -3,13 +3,51 @@
 return {
   {
     "kawre/leetcode.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "MeanderingProgrammer/render-markdown.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
     ---@module "leetcode"
     ---@type lc.UserConfig|{}
     opts = {
       lang = "cpp",
-      picker = { provider = "telescope" },
-      image = true,
+      theme = {
+        easy_alt = { link = "VirtualTextHint" },
+        medium_alt = { link = "VirtualTextWarning" },
+        hard_alt = { link = "VirtualTextError" },
+
+        easy = { link = "DiagnosticHint" },
+        medium = { link = "DiagnosticWarn" },
+        hard = { link = "DiagnosticError" },
+
+        ok = { link = "DiagnosticHint" },
+        info = { link = "DiagnosticInfo" },
+        hint = { link = "DiagnosticOk" },
+        error = { link = "DiagnosticError" },
+
+        case_ok = { link = "DiagnosticHint" },
+        case_err = { link = "DiagnosticError" },
+        case_focus_ok = { link = "VirtualTextHint" },
+        case_focus_err = { link = "VirtualTextError" },
+
+        normal = { link = "Fg" },
+        alt = { link = "Comment" },
+
+        code = { link = "@markup.raw.markdown_inline" },
+        example = { link = "@lsp.type.enum" },
+        constraints = { link = "@markup.math" },
+        header = { link = "@markup.heading" },
+        followup = { link = "@markup.link" },
+
+        indent = { link = "Whitespace" },
+        link = { link = "@markup.link.label" },
+        list = { link = "@markup.list" },
+        ref = { link = "@markup.link.url" },
+        su = {},
+      },
     },
   },
 }
