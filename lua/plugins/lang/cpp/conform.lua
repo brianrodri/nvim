fc5d502627e7c3a44053048b77fdd4412372d60f
Brian Rodriguez <brian@brianrodri.com>
2025-06-15 01:25:11 -0400

    feat(cpp): use clang-format

---
+++ lua/plugins/lang/cpp/conform.lua
@@ -0,0 +1,10 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "stevearc/conform.nvim",
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = { formatters_by_ft = { cpp = { "clang-format" } } },
+  },
+}
