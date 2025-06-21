a97c6836602e0b34ba1366830c0046448f290794
Brian Rodriguez <brian@brianrodri.com>
2025-06-20 23:19:07 -0400

    refactor(cppman): move to integ folder

---
+++ lua/plugins/integ/cppman/cppman.lua
@@ -0,0 +1,12 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "v1nh1shungry/cppman.nvim",
+    cmd = "Cppman",
+    dependencies = { "folke/snacks.nvim" },
+    ---@module "cppman"
+    ---@type cppman.Config
+    opts = { picker = "snacks" },
+  },
+}
