5fa1c10efcce2270696fb982bcf94c342cbf4d64
Brian Rodriguez <brian@brianrodri.com>
2025-06-27 21:33:44 -0400

    style: use 120 line-length for gdscript files

--- lua/plugins/lang/godot/conform.lua
+++ lua/plugins/lang/godot/conform.lua
@@ -5,6 +5,9 @@ return {
     "stevearc/conform.nvim",
     ---@module "conform"
     ---@type conform.setupOpts
-    opts = { formatters_by_ft = { gdscript = { "gdformat" } } },
+    opts = {
+      formatters_by_ft = { gdscript = { "gdformat" } },
+      formatters = { gdformat = { command = "gdformat", args = { "-", "--line-length", "120" } } },
+    },
   },
 }
