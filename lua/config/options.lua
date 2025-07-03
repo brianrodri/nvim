1d83346d0d7a11dfa294f61b3e1648c83713c09c
Brian Rodriguez <brian@brianrodri.com>
2025-07-03 03:10:48 -0400

    opts: configure default indentation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -53,6 +53,13 @@ vim.opt.confirm = true
 vim.o.textwidth = 120
 vim.o.colorcolumn = "+1"
 
+-- Use 4 spaces for indentation by default
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
 -- Use treesitter folding
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
