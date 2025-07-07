6e5bf55d85736775272566ca6b5137e3692c2459
Brian Rodriguez <brian@brianrodri.com>
2025-07-07 19:19:33 -0400

    fix(rust.conform): use nightly rustfmt via rustup

--- lua/plugins/lang/rust/conform.lua
+++ lua/plugins/lang/rust/conform.lua
@@ -5,6 +5,14 @@ return {
     "stevearc/conform.nvim",
     ---@module "conform"
     ---@type conform.setupOpts
-    opts = { formatters_by_ft = { rust = { "rustfmt" } } },
+    opts = {
+      formatters_by_ft = { rust = { "rustfmt" } },
+      formatters = {
+        rustfmt = {
+          command = "rustup",
+          prepend_args = { "run", "nightly", "--", "rustfmt" },
+        },
+      },
+    },
   },
 }
