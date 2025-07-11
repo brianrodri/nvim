04c8507a339908782f5e349aaea36faccded7e6c
Brian Rodriguez <brian@brianrodri.com>
2025-07-11 19:12:39 -0400

    feat(ui.dap-virtual-text): show runtime variable values inline while debugging code

---
+++ lua/plugins/ui/dap-virtual-text.lua
@@ -0,0 +1,11 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap" },
+    opts = {
+      enabled = true,
+    },
+  },
+}
