2c86921609f1a00e2b508380dc62c25f935ff41d
Brian Rodriguez <brian@brianrodri.com>
2025-07-11 20:04:07 -0400

    style: stop showing cursorline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,9 +39,6 @@ vim.opt.timeoutlen = 300
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 2
 
