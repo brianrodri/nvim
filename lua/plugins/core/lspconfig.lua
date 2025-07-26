d9268df25e92c824355e253f561fd91d6fd0a80d
Brian Rodriguez <brian@brianrodri.com>
2025-07-26 02:53:07 -0400

    feat(lspconfig): improve lsp documentation formatting

--- lua/plugins/core/lspconfig.lua
+++ lua/plugins/core/lspconfig.lua
@@ -1,3 +1,22 @@
+local global_lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+global_lsp_capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolvedSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
+}
+
 ---@module "lazy"
 ---@type LazySpec
 return {
@@ -5,6 +24,7 @@ return {
     "neovim/nvim-lspconfig",
     ---@param opts my.types.LspConfig|?
     config = function(_, opts)
+      vim.lsp.config("*", { capabilities = global_lsp_capabilities })
       for name, config in pairs(opts or {}) do
         if config then vim.lsp.enable(name, true) end
         if type(config) == "table" and not vim.tbl_isempty(config) then vim.lsp.config(name, config) end
