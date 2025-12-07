b43571a6a9db1d806178f19335662d2aee51625a
Brian Rodriguez <brian@brianrodri.com>
2025-12-07 08:31:01 -0500

    fix(fugitive): start in insert mode when writing new commits

--- lua/plugins/integ/git/vim-fugitive.lua
+++ lua/plugins/integ/git/vim-fugitive.lua
@@ -4,7 +4,7 @@ return {
   { -- A Git wrapper so good it should be illegal.
     "tpope/vim-fugitive",
     keys = {
-      { "<leader>gc", "<cmd>Git commit<cr>", desc = "Commit" },
+      { "<leader>gc", "<cmd>Git commit | startinsert<cr>", desc = "Commit" },
       { "<leader>gp", "<cmd>Git push<cr>", desc = "Push" },
       { "<leader>gl", "<cmd>Git pull<cr>", desc = "Pull" },
       { "<leader>gs", "<cmd>Git<cr>", desc = "Pull" },
