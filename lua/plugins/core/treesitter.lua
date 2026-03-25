d76a387584e8415ae6de1b3b8ab3fefd67c17ca7
Brian Rodriguez <brian@brianrodri.com>
2026-03-24 21:23:26 -0400

    fix(treesitter): use modern init config

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -5,19 +5,14 @@ local my_utils = require("my.utils")
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
-    main = "nvim-treesitter.configs",
-    ---@type my.types.TSConfig
-    opts = {
-      ensure_installed = {},
-      indent = { enable = true },
-      highlight = { enable = true, use_languagetree = true },
-    },
-    ---@param opts TSConfig|?
+    opts = { ensure_installed = {} },
+    opts_extend = { "ensure_installed" },
     config = function(_, opts)
-      opts = opts or {}
-      opts.ensure_installed = my_utils.dedupe(opts.ensure_installed --[[@as table<string>]] or {})
-      require("nvim-treesitter.configs").setup(opts)
+      local nvim_treesitter = require("nvim-treesitter")
+      nvim_treesitter.setup(opts)
+      nvim_treesitter.install(my_utils.dedupe(opts.ensure_installed))
     end,
   },
 }
